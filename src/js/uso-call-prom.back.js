98ad041229c9a9b40e25550fa9d0a95d3970b6c3
Eduardo Moraga <eduardom1999@gmail.com>
2023-11-03 16:01:09 -0300

    Promise Catch
    implementacion de promise.catch y promise.finally

---
+++ src/js/uso-call-prom.back.js
@@ -0,0 +1,30 @@
+import './styles.css';
+import { buscarHeroe } from './js/promesas.js';
+import { buscarHeroe as buscarHeroeCallback } from './js/callbacks.js';
+
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
+
+// buscarHeroe(heroeId, (err, heroe1) => {
+//     if (err) { return console.error(err); }
+//     buscarHeroe(heroeId2, (err, heroe2) => {
+//         if (err) { return console.error(err); }
+//         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
+//     });
+// });
+
+// buscarHeroe(heroeId1).then(heroe => {
+//     console.log(`Enviando a ${heroe.nombre} a la misión`);
+//     buscarHeroe(heroeId2).then(heroe2 => {
+//         console.log(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la misión`);
+//     });
+// });
+
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
+    .then(([heroe1, heroe2]) => {
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
+    }).catch(err => {
+        alert(err);
+    }).finally(() => {
+        console.log('Se termino el promise.all')
+    });
