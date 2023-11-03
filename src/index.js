03fcaa4025ee4124a285ce5a38ef6733f24665f3
Eduardo Moraga <eduardom1999@gmail.com>
2023-11-03 12:21:14 -0300

    Promise.all

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ import { buscarHeroe } from './js/promesas.js';
 import { buscarHeroe as buscarHeroeCallback } from './js/callbacks.js';
 
 const heroeId1 = 'capi';
-const heroeId2 = 'iron';
+const heroeId2 = 'spider';
 
 // buscarHeroe(heroeId, (err, heroe1) => {
-
 //     if (err) { return console.error(err); }
-
 //     buscarHeroe(heroeId2, (err, heroe2) => {
-
 //         if (err) { return console.error(err); }
-
 //         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
-
 //     });
 // });
 
-buscarHeroe(heroeId1).then(heroe => {
-    console.log(`Enviando a ${heroe.nombre} a la misión`);
+// buscarHeroe(heroeId1).then(heroe => {
+//     console.log(`Enviando a ${heroe.nombre} a la misión`);
+//     buscarHeroe(heroeId2).then(heroe2 => {
+//         console.log(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la misión`);
+//     });
+// });
 
-    buscarHeroe(heroeId2).then(heroe2 => {
-        console.log(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la misión`);
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
+    .then(([heroe1, heroe2]) => {
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
     });
-});
