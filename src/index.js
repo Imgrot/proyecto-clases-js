480bf4e33383897d2bf0c246ae1501b014492615
Eduardo Moraga <eduardom1999@gmail.com>
2023-11-06 17:09:51 -0300

    For Await, If Await
    mi explotó cerebro ya

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import { obtenerHeroesArr, obtenerHeroeAwait } from "./js/await";
+import { heroesCiclo, heroeIfAwait} from "./js/await";
 
-console.time('await');
-
-obtenerHeroeAwait('capia')
-    .then(heroe => {
-        console.log(heroe);
-
-        console.timeEnd('await');
-    }).catch(console.warn);
+heroeIfAwait('iron');
--- src/js/await.js
+++ src/js/await.js
@@ -1,6 +1,7 @@
 import { buscarHeroeAsync, buscarHeroe } from './promesas';
 
 const heroesIds = ['capi', 'iron', 'spider'];
+const heroesPromesas = heroesIds.map(buscarHeroe);
 
 export const obtenerHeroesArr = async () => {
 
@@ -21,11 +22,36 @@ export const obtenerHeroeAwait = async (id) => {
         const heroe = await buscarHeroeAsync(id);
         return heroe;
     }
-    catch(err){
+    catch (err) {
         console.log('CATCH!!!');
-        return{
+        return {
             nombre: 'Sin nombre',
             poder: 'Sin poder'
         }
     }
 };
+
+export const heroesCiclo = async () => {
+    console.time('HeroesCiclo');
+
+    // forma #1
+    const heroes = await Promise.all(heroesPromesas);
+    heroes.forEach(heroe => console.log(heroe));
+
+    // forma #2
+    // for await (const heroe of heroesPromesas) {
+    //     console.log(heroe);}
+
+    // forma #3 (no recomendado)
+    // heroesPromesas.forEach(async(p) => console.log(await p));
+
+    console.timeEnd('HeroesCiclo');
+}
+
+export const heroeIfAwait = async (id) => {
+    if ((await buscarHeroeAsync(id)).nombre === 'Ironman') {
+        console.log('Es el mejor de todos');
+    } else {
+        console.log('Naaaa');
+    }
+}
