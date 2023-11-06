a9ffeeb479c9cff069f02db74c1e9416c20315b7
Eduardo Moraga <eduardom1999@gmail.com>
2023-11-06 12:42:00 -0300

    Promise.race

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './styles.css';
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas.js'
+
+promesaLenta.then(console.log);
+promesaMedia.then(console.log);
+promesaRapida.then(console.log);
+
+Promise.race([promesaLenta, promesaMedia, promesaRapida])
+    .then(mensaje => console.log(mensaje))
+    .catch(console.warn);
